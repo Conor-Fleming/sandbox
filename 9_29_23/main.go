25c7f3ed14d45074e5a09ac701658a16a9cf685e
Conor-Fleming <conorfleming03@gmail.com>
2023-09-30 01:47:37 -0700

    solution

--- 9_29_23/main.go
+++ 9_29_23/main.go
@@ -7,11 +7,38 @@ type Developer struct {
 	Age  int
 }
 
-func FilterUnique(developers []Developer) []string {
-	// TODO Implement
-	return nil
-}
-
 func main() {
 	fmt.Println("Filter Unique Challenge")
+	devs := []Developer{
+		{Name: "Elliot"},
+		{Name: "Alan"},
+		{Name: "Jennifer"},
+		{Name: "Graham"},
+		{Name: "Paul"},
+		{Name: "Alan"},
+	}
+
+	fmt.Println(devs)
+	result := FilterUnique(devs)
+	fmt.Println(result)
+}
+
+func FilterUnique(developers []Developer) []string {
+	var uniqueNames []string
+	for _, v := range developers {
+		if contains(uniqueNames, v.Name) {
+			uniqueNames = append(uniqueNames, v.Name)
+		}
+	}
+
+	return uniqueNames
+}
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return false
+		}
+	}
+	return true
 }
