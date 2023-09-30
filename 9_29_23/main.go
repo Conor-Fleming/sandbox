bca66d2462c2122d4c00307ce2d028b4fea8f3c1
Conor-Fleming <conorfleming03@gmail.com>
2023-09-29 23:29:20 -0700

    starting point

---
+++ 9_29_23/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+type Developer struct {
+	Name string
+	Age  int
+}
+
+func FilterUnique(developers []Developer) []string {
+	// TODO Implement
+	return nil
+}
+
+func main() {
+	fmt.Println("Filter Unique Challenge")
+}
